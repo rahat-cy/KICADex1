57a2ca5340f02079d1edf815b6c986d60d495ce9
Rahat <mahmudrahat73@gmail.com>
2021-10-06 17:13:41 -0400

    My First Commit

---
+++ c++/test.cc
@@ -0,0 +1,20 @@
+                                       
+#include<iostream>
+using namespace std;
+int main()
+{
+string color,pluralnoun,celebrity;
+cout<<"Enter your favourite color:";
+getline(cin,color);
+cout<<"Enter a plural noun:";
+getline(cin,pluralnoun);
+cout<<"Enter your favourite celebrity:";
+getline(cin,celebrity);
+cout<<"Color is"<<color<<endl;
+cout<<pluralnoun<<"is blue"<<endl;
+cout<<"I love"<<celebrity<<endl;
+
+return 0;
+}
+
+
